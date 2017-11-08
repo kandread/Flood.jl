2d6b18a7c7ffb60f4ff4bbf6b72ee1fade035564
Kostas Andreadis <kandread@jpl.nasa.gov>
2017-11-08 14:04:58 -0800

    added boundary condition types

--- src/Flood.jl
+++ src/Flood.jl
@@ -5,4 +5,24 @@ const g = 9.81
 const alpha = 0.7
 const depth_thresh = 0.001
 
+# boundary conditions
+abstract type BC end
+
+struct HFIX <: BC
+    values :: Array{Real, 1}
+end
+
+const QFIX = HFIX
+
+const FREE = HFIX
+
+struct QVAR <: BC
+    values :: Array{Real, 1}
+    time :: Array{Int, 1}
+    prev_time :: Int
+end
+
+const HVAR = QVAR
+
+
 end
