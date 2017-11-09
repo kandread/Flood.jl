1a39cd3fdc25d4e5367c23499388aff43345877f
Kostas Andreadis <kandread@jpl.nasa.gov>
2017-11-08 17:17:42 -0800

    read parameters from file

--- src/Flood.jl
+++ src/Flood.jl
@@ -26,10 +26,10 @@ const HVAR = QVAR
 
 # domain data structure
 struct Domain
-    xul :: Float
-    yul :: Float
-    xres :: Float
-    yres :: Float
+    xul :: Float32
+    yul :: Float32
+    xres :: Float32
+    yres :: Float32
     nrows :: Int
     ncols :: Int
 end
@@ -37,13 +37,37 @@ end
 # parameter structure
 struct Params
     dem_file :: String
-    sim_time :: Float
-    init_tstep :: Float
-    fpfric :: Float
-    saveint :: Float
+    sim_time :: Float32
+    init_tstep :: Float32
+    fpfric :: Float32
+    saveint :: Float32
     bci_file :: String
-    bdy_file :: String
+    bdy_file :: Nullable{String}
+    output_dir :: Nullable{String}
+
 end
 
+function read_params(filename::String)
+    data = Dict{String, String}()
+    open(filename, "r") do f
+        for line in eachline(f)
+            if !startswith(line, "#")
+                tokens = split(line)
+                if length(tokens) > 1
+                    data[tokens[1]] = tokens[2]
+                end
+            end
+        end
+    end
+    dem_file = haskey(data, "DEMfile") ? data["DEMfile"] : nothing
+    sim_time = haskey(data, "sim_time") ? parse(Float32, data["sim_time"]) : nothing
+    init_tstep = haskey(data, "initial_tstep") ? parse(Float32, data["initial_tstep"]) : nothing
+    fpfric = haskey(data, "fpfric") ? parse(Float32, data["fpfric"]) : nothing
+    bci_file = haskey(data, "bcifile") ? data["bcifile"] : nothing
+    bdy_file = haskey(data, "bdyfile") ? data["bdyfile"] : nothing
+    saveint = haskey(data, "saveint") ? parse(Float32, data["saveint"]) : nothing
+    outdir = haskey(data, "dirroot") ? data["dirroot"] : nothing
+    return Params(dem_file, sim_time, init_tstep, fpfric, saveint, bci_file, bdy_file)
+end
 
 end
